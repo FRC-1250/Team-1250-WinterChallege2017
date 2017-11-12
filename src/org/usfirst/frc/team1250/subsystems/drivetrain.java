46775fc203c638c6e427173491960431e18b059d
Damien Fullerton <dfullerton20@hfa-dearborn.org>
2017-11-11 19:43:37 -0500

    adding subsys
    adding subsys

--- src/org/usfirst/frc/team1250/subsystems/drivetrain.java
+++ src/org/usfirst/frc/team1250/subsystems/drivetrain.java
@@ -9,37 +9,37 @@ import edu.wpi.first.wpilibj.Timer;
 import com.ctre.CANTalon;
 
 
-public class drivetrain {
-
-	SFrontRight = new CANTalon(17);
-	MFrontLeft = new CANTalon(11);
-	SFrontLeft = new CANTalon(12);
-	MBackRight = new CANTalon(16);
-	SBackRight = new CANTalon(15);
-	MBackLeft = new CANTalon(13);
-	SBackLeft = new CANTalon(14);
-
-// Setting the Slaves
-	
-	SFrontRight.changeControlMode(CANTalon.TalonControlMode.Follower);
-	SFrontLeft.changeControlMode(CANTalon.TalonControlMode.Follower);
-	SBackRight.changeControlMode(CANTalon.TalonControlMode.Follower);
-	SBackLeft.changeControlMode(CANTalon.TalonControlMode.Follower);
-
-// Setting The Masters
-	
-	SFrontRight.set(MFrontRight.getDeviceID());
-	SFrontLeft.set(MFrontLeft.getDeviceID());
-	SBackRight.set(MBackRight.getDeviceID()); 
-	SBackLeft.set(MBackLeft.getDeviceID());
-	
-	robotDrive = new RobotDrive(MFrontLeft, MBackLeft , MFrontRight, MBackRight);
-
-// Inverting Left side
-	
-	robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);
-																
-															
-	robotDrive.setInvertedMotor(MotorType.kRearLeft, true);
-															
-}
+//public class drivetrain {
+//
+//	SFrontRight = new CANTalon(17);
+//	MFrontLeft = new CANTalon(11);
+//	SFrontLeft = new CANTalon(12);
+//	MBackRight = new CANTalon(16);
+//	SBackRight = new CANTalon(15);
+//	MBackLeft = new CANTalon(13);
+//	SBackLeft = new CANTalon(14);
+//
+//// Setting the Slaves
+//	
+//	SFrontRight.changeControlMode(CANTalon.TalonControlMode.Follower);
+//	SFrontLeft.changeControlMode(CANTalon.TalonControlMode.Follower);
+//	SBackRight.changeControlMode(CANTalon.TalonControlMode.Follower);
+//	SBackLeft.changeControlMode(CANTalon.TalonControlMode.Follower);
+//
+//// Setting The Masters
+//	
+//	SFrontRight.set(MFrontRight.getDeviceID());
+//	SFrontLeft.set(MFrontLeft.getDeviceID());
+//	SBackRight.set(MBackRight.getDeviceID()); 
+//	SBackLeft.set(MBackLeft.getDeviceID());
+//	
+//	robotDrive = new RobotDrive(MFrontLeft, MBackLeft , MFrontRight, MBackRight);
+//
+//// Inverting Left side
+//	
+//	robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);
+//																
+//															
+//	robotDrive.setInvertedMotor(MotorType.kRearLeft, true);
+//															
+//}
