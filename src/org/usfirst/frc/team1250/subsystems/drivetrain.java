ac99be50199fc334d4ab316ccbe32fd23af437fc
Damien Fullerton <dfullerton20@hfa-dearborn.org>
2017-11-11 19:40:29 -0500

    adding subsys
    adding subsys

--- src/org/usfirst/frc/team1250/subsystems/drivetrain.java
+++ src/org/usfirst/frc/team1250/subsystems/drivetrain.java
@@ -1,12 +1,45 @@
 package org.usfirst.frc.team1250.subsystems;
 
-import org.usfirst.frc.team1250.robot.Robot;
-import com.ctre.CANTalon;
-
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+import com.ctre.CANTalon;
 
-public class drivetrain {	
+
+public class drivetrain {
+
+	SFrontRight = new CANTalon(17);
+	MFrontLeft = new CANTalon(11);
+	SFrontLeft = new CANTalon(12);
+	MBackRight = new CANTalon(16);
+	SBackRight = new CANTalon(15);
+	MBackLeft = new CANTalon(13);
+	SBackLeft = new CANTalon(14);
+
+// Setting the Slaves
 	
+	SFrontRight.changeControlMode(CANTalon.TalonControlMode.Follower);
+	SFrontLeft.changeControlMode(CANTalon.TalonControlMode.Follower);
+	SBackRight.changeControlMode(CANTalon.TalonControlMode.Follower);
+	SBackLeft.changeControlMode(CANTalon.TalonControlMode.Follower);
+
+// Setting The Masters
 	
+	SFrontRight.set(MFrontRight.getDeviceID());
+	SFrontLeft.set(MFrontLeft.getDeviceID());
+	SBackRight.set(MBackRight.getDeviceID()); 
+	SBackLeft.set(MBackLeft.getDeviceID());
+	
+	robotDrive = new RobotDrive(MFrontLeft, MBackLeft , MFrontRight, MBackRight);
+
+// Inverting Left side
+	
+	robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);
+																
+															
+	robotDrive.setInvertedMotor(MotorType.kRearLeft, true);
+															
 }
